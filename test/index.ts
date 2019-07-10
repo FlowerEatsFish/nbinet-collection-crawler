90887e1deed024bea0c87db17a26ef4aecae98bf
Eating-Fish Flower <flower.eating.fish@gmail.com>
2019-07-10 21:36:19 +0800

    Fix fails in unit test

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,11 @@
 import nbinetCollection from '../src/index';
+import { DetailType } from '../index';
 
 const timeout = 60 * 1000;
 
 describe('Run demo', (): void => {
   it('Should get one result as Object', async (): Promise<void> => {
-    const result: object = await nbinetCollection(
+    const result = await nbinetCollection(
       '護理學博士教育課程規劃與評值研討會',
       'keyword'
     );
@@ -15,19 +16,19 @@ describe('Run demo', (): void => {
   }, timeout);
 
   it('Should get results as Array and no more than 50', async (): Promise<void> => {
-    const result: object[] = await nbinetCollection(
+    const result = await nbinetCollection(
       '9789867494122',
       'isbn'
     );
 
     expect(Array.isArray(result)).toBeTruthy();
-    expect(result.length).toBeLessThanOrEqual(50);
+    expect((result as DetailType[]).length).toBeLessThanOrEqual(50);
   }, timeout);
 
   it('Should do not have any result as Null', async (): Promise<void> => {
-    const result: null = await nbinetCollection(
+    const result = await nbinetCollection(
       'blablablablablablablablablablablabla',
-      'keyowrd'
+      'keyword'
     );
 
     expect(result).toBeNull();
